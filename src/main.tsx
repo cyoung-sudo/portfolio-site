70fdfde4e71a880764f2ea34a74e0834b2bfce9c
Christopher Young <christopheryoung@MacBook123.local>
2024-10-23 13:05:35 -0700

    basic layout, sass, & bootstrap setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import App from './App.tsx'
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
